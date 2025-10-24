0888749e5938ae06c061e84af53812cbc4214b8a
ptr kwng <ptrlkwng@hotmail.com>
2025-10-24 22:13:22 +0800

    fixed js removeeventlistener issues, in-between cases for images 550px t0 700px

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-const openButton = document.getElementById('open-sidebar-button');
+const openButton = document.getElementById('open-menu-button');
 const navbar = document.getElementById('navbar');
 const media = window.matchMedia("(width < 700px)");
-
-/* A combination of tips from: https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event */
-/* and https://www.onlywebpro.com/tutorials/javascript/optimize-scrolling-performance-by-debouncing-scroll-event-calls */
-var debounce_timer;
-window.addEventListener("scroll", (e) => {
-    if (debounce_timer) {
-        window.clearTimeout(debounce_timer);
-        console.log('inside onscroll function but not closing yet...');
-    }
-    debounce_timer = window.setTimeout(function () {
-        // run your actual function here
-        closeSidebar()
-        updateNavbar(e);
-        console.log('...gone through setTimeout and inert is ' + navbar.hasAttribute('inert'));
-    }, 100);
-});
-/* tip ends here */
 
 /* if the Escape key is pressed with the mobile menu deployed, the menu is closed */
 window.addEventListener("keyup", (e) => {
     console.log('pressed ' + e.key);
-    console.log(openButton.getAttribute('aria-expanded'));
-    if (e.key === 'Escape') {
-        closeSidebar();
+    if (e.key === 'Escape' && openButton.getAttribute('aria-expanded') === 'true') {
+        /* console.log('menu is open = ' + openButton.getAttribute('aria-expanded')); */
+        closeMenu();
         updateNavbar(e);
     }
+    /* console.log('menu currently is ' + (openButton.getAttribute('aria-expanded') === 'true' ? 'open' : 'closed')); */
 });
 
 /* code within setTimeout is to prevent the 'flash' of the mobile menu as viewport is resized */
+/* without it, as the viewport is resized, inert renders the menu items unselectable */
 media.addEventListener('change', (e) => {
-    console.log('We\'ve moved in/out of 700px: ', e.target);
+    /* console.log('We\'ve moved in/out of 700px: ', e.target); */
     setTimeout(function () {
         navbar.classList.remove('animate', 'show');
     }, 300);
@@ -40,30 +25,54 @@ media.addEventListener('change', (e) => {
 
 function updateNavbar(e) {
     const isMobile = e.matches;
-    console.log('isMobile=' + isMobile);
+    /* console.log('isMobile=' + isMobile); */
     if (isMobile || window.innerWidth < 700) {
         navbar.setAttribute('inert', '');
-        console.log('inert is ' + navbar.hasAttribute('inert'));
+        /* console.log('inert is ' + navbar.hasAttribute('inert')); */
     }
     else {
         // desktop mode
+        window.removeEventListener("scroll", scrollEventHandler);
         navbar.removeAttribute('inert');
-        console.log('inert is ' + navbar.hasAttribute('inert'));
+        /* console.log('inert is ' + navbar.hasAttribute('inert')); */
     }
 }
 
-function openSidebar() {
+/* A combination of tips from: https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event */
+/* and https://www.onlywebpro.com/tutorials/javascript/optimize-scrolling-performance-by-debouncing-scroll-event-calls */
+var debounce_timer;
+function scrollEventHandler() {
+    if (debounce_timer) {
+        window.clearTimeout(debounce_timer);
+        console.log('inside scrollEventHandler but not closing yet...debounce_timer is ' + debounce_timer);
+    }
+    /* window.removeEventListener("scroll", scrollEventHandler); Better to remove here and move down in closeSidebar*/
+    /* closeMenu() can also be here */
+    debounce_timer = window.setTimeout(function () {
+        // run your actual function here
+        closeMenu();
+        /* console.log(debounce_timer); */
+        /* console.log('...gone through setTimeout and inert is ' + navbar.hasAttribute('inert')); */
+    }, 50);
+}
+/* tip ends here */
+
+
+/* FIXED: there's case where if the click is outside the menu, it closes but eventlistener is still active */
+function openMenu() {
     navbar.classList.add('animate', 'show');
     openButton.setAttribute('aria-expanded', 'true');
     navbar.removeAttribute('inert');
-    console.log('sidebar open and inert is ' + navbar.hasAttribute('inert'));
+    window.addEventListener("scroll", scrollEventHandler);
+    /* console.log('menu open and inert is ' + navbar.hasAttribute('inert')); */
 }
 
-function closeSidebar() {
+function closeMenu() {
     navbar.classList.remove('show');
     openButton.setAttribute('aria-expanded', 'false');
     navbar.setAttribute('inert', '');
-    console.log('sidebar is closed and inert is ' + navbar.hasAttribute('inert'));
+    window.removeEventListener("scroll", scrollEventHandler);
+    /* console.log('menu is closed and inert is ' + navbar.hasAttribute('inert')); */
 }
 
-/* updateNavbar(media) */
+updateNavbar(media);
